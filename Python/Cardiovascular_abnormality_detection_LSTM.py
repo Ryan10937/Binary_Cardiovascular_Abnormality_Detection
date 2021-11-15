ef11e5ab62ca4405d4e54f58bf2caa44b6edc8ed
Ryan10937 <76846047+Ryan10937@users.noreply.github.com>
2021-11-15 11:51:02 -0600

    Added ANN and LSTM py files

---
+++ Python/Cardiovascular_abnormality_detection_LSTM.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %%
+
+# %%
+import sys
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+def get_freq_bins(signal,fs,num_bins,bin_width,verbose=False):
+#function that returns num_bins number of bins with width bin_width 
+    size = np.shape(signal)[1]
+    print("size:",size)
+    ts = 1/fs
+    amplidtude = np.fft.fft(signal,size)/size
+    df=fs/size; #frequency resolution
+    f=[i*df for i in range(0,int(size/2))]
+    if verbose:
+        plt.figure(1)
+        plt.subplot(2,1,1)
+        plt.stem(f,abs(frequencyArr[0:int(size/2)]))
+        plt.subplot(2,1,2)
+        plt.plot(signal)
+
+    bin_array = np.array(0,dtype = float)
+    
+    for i in range(0,num_bins*bin_width,bin_width):
+        #first index of bin array is the amplitudes from fq 1 to 5. second is 5 to 10 and so on
+        bin_array = np.append(bin_array, np.sum(abs(amplidtude[i:i+bin_width+1])))
+    bin_array = np.delete(bin_array,0)
+    #Make graphs of time series and frequency and save those images as .png
+    #Make parameter to pass patient ID save as patient_ID_FFT.png patient_ID_TIMESERIES.png
+    #add validation dataset
+    return bin_array
+
+
+def Run_CAD_LSTM_Model(model_path, csv_path, sampling_frequency,patient_ID, date,result_path):
+    
+    model = tf.keras.models.load_model(model_path)
+    
+    raw_ecg =  pd.read_csv(csv_path)
+    
+    bins=76
+    width = 1
+    raw_ecg = raw_ecg.to_numpy()
+    fft_arr = get_freq_bins(raw_ecg,int(sampling_frequency),bins,width)
+    
+    #train = train.reshape((train.shape[0],train.shape[1],1))
+
+    
+    #result = model.predict(fft_arr.reshape(1,1,bins))
+    #result = model.predict(fft_arr.reshape(bins,1,1))
+    result = model.predict(fft_arr.reshape(1,bins,1))
+
+    f = open(result_path + patient_ID+ '_' + date + "_results.txt", "w")
+
+    if result[0][0] > result[0][1]:
+        #print to result file
+        print('patient normal')
+        f.write('patient normal')
+    else:
+        print('patient abnormal')
+        f.write('patient abnormal')
+        
+    print(result)
+    
+
+if __name__ == '__main__':
+    
+    if(len(sys.argv) != 7):
+        print("Wrong number of arguments. Should be\nmodel_path, csv_path, sampling_frequency,patient_ID, date")
+    Run_CAD_LSTM_Model(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    
+
+
+
